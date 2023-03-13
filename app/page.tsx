9f794305c1b3b4dceaac5674136682f0a7137132
Fernando Navarro <fernandonavarro@berkeley.edu>
2023-03-12 19:52:01 -0700

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+type Props = {};
+
+export default function HomePage({}: Props) {
+  return (
+    <div>
+      <h1>Page</h1>
+      <p>Content</p>
+    </div>
+  );
+}
